96b483cb59a13de82ff0ade4e302c4312bf6a575
prawas <sprawas@gmail.com>
2019-08-28 18:31:23 +0300

    404

--- 404.php
+++ 404.php
@@ -1,1 +1,11 @@
-404
+<div class="section section_404">
+    <div class="container">
+        <div class="center-block" style="max-width: 1000px;">
+            <div class="content-404">
+                <h1 class="section__title content-404__title">404</h1>
+                <div class="content-404__text">Запрашиваемая вами страница была перемещена или удалена.</div>
+                <div><a class="btn btn-lg btn-secondary" href="/">На главную</a></div>
+            </div>
+        </div>
+    </div>
+</div>
