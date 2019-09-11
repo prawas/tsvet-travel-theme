2a247e4c282cc2523ed3981db39e2867ec5cb3a9
prawas <sprawas@gmail.com>
2019-09-11 15:09:43 +0300

    titles on simple pages

--- page-gut.php
+++ page-gut.php
@@ -7,6 +7,7 @@
 <section class="section section_gutenberg">
     <div class="section__container container">
     <?php the_post(); ?>
+    <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
     </div>
 </section>
