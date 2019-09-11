24afc386982df6307faef664503f676b327e8d45
prawas <sprawas@gmail.com>
2019-09-11 14:19:29 +0300

    simple page template

---
+++ page-gut.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Страница без конструктора
+ */
+?>
+
+<section class="section section_gutenberg">
+    <div class="section__container container">
+    <?php the_post(); ?>
+    <?php the_content(); ?>
+    </div>
+</section>
